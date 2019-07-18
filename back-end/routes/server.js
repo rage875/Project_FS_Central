d1c7f7ae574563988cb0564993e5ae4350edea86
Luis03 <rage.875@gmail.com>
2019-07-17 19:48:52 -0500

    back:server body-parser, cors header middleware, post added

--- back-end/routes/server.js
+++ back-end/routes/server.js
@@ -1,5 +1,7 @@
 // Require express package
 const express = require("express");
+// Require body-parser package
+const bodyParser = require("body-parser");
 
 // Get port from command line
 const port = process.argv[2];
@@ -7,10 +9,32 @@ const port = process.argv[2];
 // Create express application
 const app = express();
 
+// Use body parser as middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+// Add header for for 'Access-Control-Allow-Origin
+app.use("*", (req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET", "POST");
+  res.header("Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  next();
+})
+
 // Get method
 app.get("/", (req, res) =>{
+  console.log("GET methods")
   res.send("GET method from root");
 });
+
+// Post method
+app.post("/", (req, res) =>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(`POST User: ${username}, Pass: ${password}`);
+  res.status(200).end;
+})
 
 // Remain listening from the port
 app.listen(port, ()=>{
